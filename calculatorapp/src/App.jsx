a24df34d6b9b41880c2e1a0f665b1ab9e7c2469e
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-01-09 10:16:39 +0530

    done

---
+++ calculatorapp/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calculator from "./components/calculator";
+
+function App() {
+  return (
+    <>
+      <h1>Calculator App</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
---
+++ calculatorapp/src/components/Deliver.jsx
@@ -0,0 +1,5 @@
+const Deliver = (props) => {
+  return <div className="value">{props.inputs}</div>;
+};
+
+export default Deliver;
